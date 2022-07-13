227bbcffd15451eaf4fd6f0ff6aa51077cf125c9
Ihar Skavysh <igogoblin@gmail.com>
2022-07-13 14:28:44 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+function towelSort(matrix) {
+    let ar = [];
 
-// You should implement your task here.
+    if (matrix.length == 0) {
+        return [];
+    } else if ((matrix.length = 1)) {
+        matrix = matrix[0];
+    }
+    const flatArray = function (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let cur = arr[i];
+            if (typeof cur !== "number") {
+                flatArray(cur);
+            } else {
+                ar.push(cur);
+            }
+        }
+    };
 
-module.exports = function towelSort (matrix) {
-  return [];
+    const flatArrayReverse = function (arr) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let cut = arr[i];
+            if (typeof cut !== "number") {
+                flatArrayReverse(cut);
+            } else {
+                ar.push(cut);
+            }
+        }
+    };
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 != 0) {
+            flatArrayReverse(matrix[i]);
+        } else {
+            flatArray(matrix[i]);
+        }
+    }
+
+    return ar;
 }
+
+module.exports = function towelSort(matrix) {
+    return matrix;
+};
